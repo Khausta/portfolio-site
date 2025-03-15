56acd8f35a4333e9ec9cd953b85f45ea8e8b51d1
Khausta <anna.a.khaustova@gmail.com>
2025-03-15 13:17:35 +0300

    fixed promo and projects. Added media queries

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
-document.addEventListener('mousemove', e => {
-    Object.assign(document.documentElement, {
-        style: `
-        --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-        --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
-        `
+const mobileWidthMediaQuery = window.matchMedia('(max-width: 1024px)');
+
+function checkWidthMediaQuery(mediaQuery) {
+    if (mediaQuery.matches) {
+        console.log('width more then 1024px');
+        document.querySelector('.layer-10').classList.add('layer-10_animated');
+    } else {
+        addMousemoveEventListener();
+    }
+}
+
+checkWidthMediaQuery(mobileWidthMediaQuery);
+
+function addMousemoveEventListener() {
+    document.addEventListener('mousemove', e => {
+        Object.assign(document.documentElement, {
+            style: `
+            --move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+            --move-y: ${(e.clientY - window.innerHeight / 2) * -.01}deg;
+            `
+        })
     })
-})
+}
